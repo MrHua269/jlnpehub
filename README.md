7b216fc3e8056b2367ea984084a1c29aad18a80d
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 19:13:54 +0800

    清理文件

--- README.md
+++
@@ -1,4 +0,0 @@
-# wangxyper.github.io
-
-<h1>eeeee</h1>
-<h1>这个web中也包含了我们班一个同学写的一些东西</h1>
--- startHMCL.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo "Starting HMCL..."
-@echo "正在启动HMCL..."
-java -Xmx1G -Xms100M -client -jar launcher.jar
-pause
-exit 
